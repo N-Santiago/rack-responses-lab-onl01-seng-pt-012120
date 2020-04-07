9547bc57c54846746e1cad6888d61b9605a4c311
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-07 20:59:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,8 @@ class Application
     if Time.now.hour.between?(0..11)
     resp.write "Good Morning!"
     elsif Time.now.hour.between?(12..17)
+    resp.write "Good Afternoon!"
+  else
     
     resp.finish
   end
