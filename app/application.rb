e6a452d9a7d2842cb30ee9ed4035cb4a57992581
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-07 20:57:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now.hour.between?0..
+    if Time.now.hour.between?(0..12)
     resp.write "Good Morning!"
     resp.finish
   end
